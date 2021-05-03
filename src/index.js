c683f971bc6052abab123276d86db6c5ecc3d441
jacob <4974971+jscotroby@users.noreply.github.com>
2021-05-02 21:56:05 -0700

    Sum test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import readInput from "./readInput.js";
 
-const main = async () => {
+const readName = async () => {
     let name = await readInput("what's your name?");
     console.log("Your name is " + name);
 };
+
+const main = async () => {
+    let number1 = Number(await readInput("give me a number!"));
+    let number2 = Number(await readInput("give me another number!"));
+    let number3;
+    // Do something here....
+
+    console.log(number1 + "+" + number2 + "=" + number3);
+
+}
 
 
 await main();
