3897ab5412c8a678d6e04f660c3d29c6e4dac2dc
sabrowski <sabrowsk044@gmail.com>
2021-05-02 22:43:23 -0700

    on my own take 1

--- src/index.js
+++ src/index.js
@@ -8,10 +8,14 @@ const readName = async () => {
 const main = async () => {
     let number1 = Number(await readInput("give me a number!"));
     let number2 = Number(await readInput("give me another number!"));
-    let number3 = Number(number1 + number2);
-    // Do something here....
+    let number3 = number1 + number2;
+    let number4 = number3 * 10
+
+
 
     console.log(number1 + "+" + number2 + "=" + number3);
+    console.log("But" + " " + number4 + ", " + "is ten times as large")
+
 
 }
 
