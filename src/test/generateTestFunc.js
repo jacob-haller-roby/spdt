d159fc1c82615b2126c3299eb089cfdccea57fc1
jacob <4974971+jscotroby@users.noreply.github.com>
2021-05-05 10:54:04 -0700

    Colors on test output

--- src/test/generateTestFunc.js
+++ src/test/generateTestFunc.js
@@ -1,3 +1,10 @@
+const color = (function (colors) {
+    const fn = (code, str) => `\x1b[${code}m${str}\x1b[39m`;
+    const obj = { grey: fn.bind(null, 90) };
+    for (let i = 0; i < colors.length; i++) obj[colors[i]] = fn.bind(null, 30 + i);
+    return obj;
+})(['black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']);
+
 const generateTestFunc = (func) => {
     let failedTests = 0;
     let passedTests = 0;
@@ -9,20 +16,24 @@ const generateTestFunc = (func) => {
             if (output !== expectedOutput) {
                 failedTests++;
                 console.error(
-                    `\nInvalid output with inputs: ${inputString}.  \nExpected ${typeof expectedOutput} "${expectedOutput}" but got ${typeof output} "${output}"\n`
+                    color.red(`\nInvalid output with inputs: ${inputString}.`) +
+                    `\nExpected ${typeof expectedOutput} "${expectedOutput}" but got ${typeof output} "${output}"\n`
                 );
                 return;
             }
             passedTests++;
-            console.log(`\nCorrect output!: inputs: ${inputString}. output: ${output}\n`);
+            console.log(
+                color.green(`\nCorrect output!`) +
+                `: inputs: ${inputString}. output: ${output}\n`,
+                );
             return;
         },
         logResults: () => {
             console.log(`${passedTests} passed out of ${passedTests + failedTests} total tests`);
             if (failedTests === 0) {
-                console.log('Code passed all tests! :D');
+                console.log(color.green('Code passed all tests! :D'));
             } else {
-                console.error(`${failedTests} tests failed! :(`);
+                console.error(color.red(`${failedTests} tests failed! :(`));
             }
         },
     };
